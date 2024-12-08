999af3ec8260610b579f3bc12e2da75194d208da
thanh thuy <thanhthuyy1010@gmail.com>
2024-12-08 12:49:20 +0700

    12:49

--- js/admin.js
+++ js/admin.js
@@ -395,6 +395,7 @@ function toast({ title = "", message = "", type = "", duration = 5000 }) {
   const main = document.getElementById("toast");
   if (main) {
     const toast = document.createElement("div");
+
     toast.onclick = function (e) {
       if (e.target.closest(".toast__close")) {
         main.removeChild(toast);
@@ -409,18 +410,26 @@ function toast({ title = "", message = "", type = "", duration = 5000 }) {
 
     toast.classList.add("toast", `toast--${type}`);
     toast.innerHTML = `
-        <div class="toast__icon">
-          <i class="${icon}"></i>
-        </div>
-        <div class="toast__body">
-          <h3 class="toast__title">${title}</h3>
-          <p class="toast__msg">${message}</p>
-        </div>
-        <div class="toast__close">
-          <i class="fa-solid fa-xmark"></i>
-        </div>
+      <div class="toast__icon">
+        <i class="${icon}"></i>
+      </div>
+      <div class="toast__body">
+        <h3 class="toast__title">${title}</h3>
+        <p class="toast__msg">${message}</p>
+      </div>
+      <div class="toast__close">
+        <i class="fa-solid fa-xmark"></i>
+      </div>
     `;
     main.appendChild(toast);
+
+    // Sau khi hết thời gian, ẩn toast và tắt sự kiện tương tác
+    setTimeout(() => {
+      toast.classList.add("hide");
+      setTimeout(() => {
+        main.removeChild(toast);
+      }, 500); // Sau 500ms, toast sẽ bị xóa khỏi DOM
+    }, duration);
   }
 }
 
